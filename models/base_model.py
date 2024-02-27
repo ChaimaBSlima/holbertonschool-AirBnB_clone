4016cb27ee1cfffe6eafb058487051b82664c4d3
ChaimaBSlima <chaimabslima@outlook.fr>
2024-02-27 10:45:24 +0100

    task 3

---
+++ models/base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import models
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """Represents the BaseModel """
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+    def save(self):
+        """ updated_at take the current datetime."""
+        self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        """
+        Return the dictionary of the BaseModel instance.
+        """
+        chaima = self.__dict__.copy()
+        chaima["__class__"] = self.__class__.__name__
+        chaima["created_at"] = self.created_at.isoformat()
+        chaima["updated_at"] = self.updated_at.isoformat()
+
+        return chaima
+
+    def __str__(self):
+        """Return the string representation of the BaseModel instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+testing the base model
+"""
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    def testing_init(self):
+        """
+        Test for __init__
+        """
+        chaima = BaseModel()
+        self.assertIsNotNone(chaima.id)
+        self.assertIsNotNone(chaima.created_at)
+        self.assertIsNotNone(chaima.updated_at)
+
+    def testing_save(self):
+        """
+        Test for save
+        """
+        chaima = BaseModel()
+        upat = chaima.updated_at
+        cupat = chaima.save()
+        self.assertNotEqual(upat, cupat)
+
+    def testing_to_dict(self):
+        """
+        Test for to_dict
+        """
+        chaima = BaseModel()
+        mdict = chaima.to_dict()
+        self.assertIsInstance(mdict, dict)
+        self.assertEqual(mdict["__class__"], 'BaseModel')
+        self.assertEqual(mdict['id'], chaima.id)
+        self.assertEqual(mdict['created_at'], chaima.created_at.isoformat())
+        self.assertEqual(mdict["updated_at"], chaima.created_at.isoformat())
+
+    def test_str(self):
+        """
+        Test for string representation
+        """
+        chaima = BaseModel()
+        self.assertTrue(str(chaima).startswith('[BaseModel]'))
+        self.assertIn(chaima.id, str(chaima))
+        self.assertIn(str(chaima.__dict__), str(chaima))
+
+
+if __name__ == "__main__":
+    unittest.main()
