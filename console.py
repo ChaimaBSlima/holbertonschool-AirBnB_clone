df3ff4805f2ca67e63a1068a48030c457127e5a7
ChaimaBSlima <chaimabslima@outlook.fr>
2024-02-29 12:04:38 +0100

    task 6

---
+++ console.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module for console
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    control the command
+    """
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program.
+        """
+        return True
+
+    def help_quit(self, arg):
+        """
+        print the act of quit
+        """
+        print("Quit command to exit the program")
+
+    def do_EOF(self, arg):
+        """
+        EOF (Ctrl+D) signal to exit the program.
+        """
+        print()
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
