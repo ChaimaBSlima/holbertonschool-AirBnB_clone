afcb51787c3f072d00a680b92de5674ae0992d36
ChaimaBSlima <chaimabslima@outlook.fr>
2024-02-29 11:44:59 +0100

    trying task 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -19,10 +20,12 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
+        models.storage.new(self)
 
     def save(self):
         """ updated_at take the current datetime."""
         self.updated_at = datetime.utcnow()
+        models.storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+    manages serialization and deserialization of objects
+    and from JSON files.
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def new(self, obj):
+        """
+        Adds an object to the __objects dictionary.
+        """
+        obj_name = obj.__class__.__name__
+        key = "{}.{}".format(obj_name, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def all(self):
+        """
+        Returns the dictionary __objects.
+        """
+        return FileStorage.__objects
+
+    def save(self):
+        """
+        Serializes __objects to a JSON file.
+        """
+        objects = self.__objects
+        dic_data = {}
+        for i in objects.keys():
+            dic_data[i] = objects[i].to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            json.dump(dic_data, f)
+
+    def reload(self):
+        """
+        Deserializes JSON file to __objects.
+        """
+        if os.path.isfile(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                try:
+                    dic_data = json.load(f)
+                    for key, value in dic_data.items():
+                        class_name, obj_id = key.split('.')
+                        cls = eval(class_name)
+                        instance = cls(**value)
+                        FileStorage.__objects[key] = instance
+                except Exception:
+                    pass
